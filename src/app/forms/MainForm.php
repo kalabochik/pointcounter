7b4dd24804c926685429a661c060ebe0f6493746
Roman Kuzmenko <68807893+kalabochik@users.noreply.github.com>
2025-11-05 20:13:12 +0500

    Add files via upload

---
+++ src/app/forms/MainForm.php
@@ -0,0 +1,109 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event button_plus_player1.click-Left 
+     */
+    function doButton_plus_player1ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button_minus_player1.click-Left 
+     */
+    function doButton_minus_player1ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button_minus_player2.click-Left 
+     */
+    function doButton_minus_player2ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button_minus_player3.click-Left 
+     */
+    function doButton_minus_player3ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+
+
+
+    /**
+     * @event button_plus_player2.click-Left 
+     */
+    function doButton_plus_player2ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button_plus_player3.click 
+     */
+    function doButton_plus_player3Click(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button_biscuits_player2.click-Left 
+     */
+    function doButton_biscuits_player2ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button_biscuits_player3.click-Left 
+     */
+    function doButton_biscuits_player3ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event changeNames.click-Left 
+     */
+    function doChangeNamesClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event saveNames.click-Left 
+     */
+    function doSaveNamesClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button_biscuits_player1.click-Left 
+     */
+    function doButton_biscuits_player1ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event buttonAlt.click-Left 
+     */
+    function doButtonAltClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+}
